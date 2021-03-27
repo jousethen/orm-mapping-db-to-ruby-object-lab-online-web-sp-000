4a7d6fd3075964278968a427aef7f446262a4a5f
Jouse then <jthen93@gmail.com>
2021-03-27 18:00:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,28 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.students_below_12th_grade
+    sql= <<-SQL
+          SELECT * FROM students WHERE grade <> '12'
+          SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_X_students_in_grade_10(x)
+    sql= <<-SQL
+          SELECT * FROM students WHERE grade = '10'
+          LIMIT ?
+          SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
