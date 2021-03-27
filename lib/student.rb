7c162bdba6568300427a4a3b26fafdcb55eefae4
Jouse then <jthen93@gmail.com>
2021-03-27 17:41:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,10 +10,18 @@ class Student
   end
 
   def self.all
-    sql = <<-SQL
+    sql= <<-SQL
           SELECT * FROM students
-          
-          SQL 
+          SQL
+ 
+    studnets = DB[:conn].execute(sql)
+    
+    student.each do |row|
+    end
+    
+    
+  end
+    
     
   end
 
